eb58cf5b2eceac280db8b85ddceda36adbbd6330
Eric Mooney <eric.c.mooney@gmail.com>
2013-05-30 00:55:44 -0700

    Insert all correct loops, missing validation for floats and missing re-run ability

--- cal.rb
+++ cal.rb
@@ -12,7 +12,7 @@ def calcit
 		          while true
 		          	puts "What is your first input number?"
 		            first_num = gets.chomp
-		            if (first_num.class = "Float" || first_num.class = "Fixnum") #may cause error with large numbers...
+		            if (first_num != "Float" || first_num.class != "Fixnum") #since gets becomes a string - need to find new solution to validate
 		              while true 
 		              	puts "Would you like to add, subtract, multiply, or divide that number?"
 		            	  function = gets.chomp.downcase
@@ -20,7 +20,7 @@ def calcit
 		                  while true 
 		                  	puts "What would you like to #{function} from #{first_num}"
 		                    second_num = gets.chomp
-		                    if (second_num.class = "Float" || second_num.class = "Fixnum") #may cause error with large numbers...
+		                    if (second_num.class != "Float" || second_num.class != "Fixnum") #since gets becomes a string - need to find new solution to validate
 	               	        if function == "add"
 	                       	  puts (first_num.to_f + second_num.to_f)
 	                   	    elsif function == "subtract"
@@ -63,7 +63,7 @@ def calcit
 		          while true
 		          	puts "What number are you going to use for the advanced calculator?"
 		            adv_num = gets.chomp
-		            if (adv_num.class = "Float" || adv_num.class = "Fixnum") #may cause error with large numbers...
+		            if (adv_num.class != "Float" || adv_num.class != "Fixnum") #since gets becomes a string - need to find new solution to validate
 		              while true
 		              	puts 'Would you like to find the "square" or the "square root"?'
 			              adv_answer = gets.chomp.downcase
